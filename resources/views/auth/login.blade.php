acf479084ceb523ee1e996f48a80a5e1a418a6df
Kreshna Putra Adi Wicaksana <kreshnaputraadi31@student.uns.ac.id>
2023-09-11 21:03:54 +0700

    Add Notification

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,22 +6,20 @@ Sign in
 <h3 class="kpaw_weight--extra-bold pt-0 mt-0">Sign in</h3>
 <p class="mb-3 pb-2">Masukkan alamat email dan password.</p>
 
-{{-- @if (session()->has('error'))
-<div class="kpaw_alert kpaw_alert--danger">
-    {{ session('error') }}
+@if (session('verifikasi_success'))
+<div class="alert alert-success alert-dismissible verifikasi-alert" role="alert" id="verifikasi">
+    {{ session('verifikasi_success') }}
 </div>
 @endif
 
-@if (isset($_COOKIE['login-status']))
-<div class="kpaw_alert kpaw_alert--danger" id="throttle-alert" data-throttle-end="{{ $_COOKIE['throttle-end'] }}">
-    Anda sudah melakukan 5 kali percobaan gagal. Silahkan tunggu setelah <span id="minutes">00</span> menit <span
-        id="seconds">00</span> detik.
-    Jika Anda mencoba login pada waktu tersebut dan gagal, maka IP Anda akan di Banned.
+@if (session('verifikasi_error'))
+<div class="alert alert-danger alert-dismissible verifikasi-alert" role="alert" id="verifikasi">
+    {{ session('verifikasi_error') }}
 </div>
-@endif --}}
+@endif
 
 @if (session('register_success'))
-<div class="alert alert-success" role="alert">
+<div class="alert alert-success alert-dismissible verifikasi-alert" role="alert" id="verifikasi">
     {{ session('register_success') }}
 </div>
 @endif
@@ -54,49 +52,62 @@ Sign in
         @enderror
     </div>
 
-        <button anim="ripple" type="submit" class="btn kpaw_btn kpaw_btn--primary kpaw_weight--bold w-100">
-            Sign in
-        </button>
+    <button anim="ripple" type="submit" class="btn kpaw_btn kpaw_btn--primary kpaw_weight--bold w-100">
+        Sign in
+    </button>
 
-        <div class="custom-control custom-checkbox kpaw_custom--checkbox mt-3 mb-2">
-            <div class="row no-gutters">
-                <div class="col-6">
-                    <div class="text" style="margin-left: -35px">
-                        <a href="{{ route('password.request') }}" class="kpaw_weight--bold kpaw_text--secondary">Forgot
-                            Password?</a>
-                    </div>
+    <div class="custom-control custom-checkbox kpaw_custom--checkbox mt-3 mb-2">
+        <div class="row no-gutters">
+            <div class="col-6">
+                <div class="text" style="margin-left: -35px">
+                    <a href="{{ route('password.request') }}" class="kpaw_weight--bold kpaw_text--secondary">Forgot
+                        Password?</a>
                 </div>
-                <div class="col-6">
-                    <div class="text-right">
-                        <a href="{{ route('register') }}" class="kpaw_weight--bold kpaw_text--secondary">Belum Punya Akun?</a>
-                    </div>
+            </div>
+            <div class="col-6">
+                <div class="text-right">
+                    <a href="{{ route('register') }}" class="kpaw_weight--bold kpaw_text--secondary">Belum Punya
+                        Akun?</a>
                 </div>
             </div>
         </div>
+    </div>
 </form>
 @endsection
 @push('script')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://moment.github.io/luxon/global/luxon.min.js"></script>
 <script>
     if ($("#throttle-alert").length) {
-            const DateTime = luxon.DateTime;
-            const throttle_end = $("#throttle-alert").data('throttle-end');
-            let countDownDate = DateTime.fromSQL(throttle_end).toMillis();
+        const DateTime = luxon.DateTime;
+        const throttle_end = $("#throttle-alert").data('throttle-end');
+        let countDownDate = DateTime.fromSQL(throttle_end).toMillis();
 
-            setInterval(() => {
-                let now = DateTime.now().toMillis();
-                let distance = countDownDate - now;
+        setInterval(() => {
+            let now = DateTime.now().toMillis();
+            let distance = countDownDate - now;
 
-                let minutes = Math.floor((distance % (400 * 60 * 60)) / (400 * 60));
-                let seconds = Math.floor((distance % (400 * 60)) / 400);
+            let minutes = Math.floor((distance % (400 * 60 * 60)) / (400 * 60));
+            let seconds = Math.floor((distance % (400 * 60)) / 400);
 
-                minutes < 10 ? $("#minutes").html(`0${minutes}`) : $("#minutes").html(minutes);
-                seconds < 10 ? $("#seconds").html(`0${seconds}`) : $("#seconds").html(seconds);
+            minutes < 10 ? $("#minutes").html(`0${minutes}`) : $("#minutes").html(minutes);
+            seconds < 10 ? $("#seconds").html(`0${seconds}`) : $("#seconds").html(seconds);
 
-                if (distance < 0) {
-                    $("#throttle-alert").remove();
-                }
-            }, 1000);
-        }
+            if (distance < 0) {
+                $("#throttle-alert").remove();
+            }
+        }, 1000);
+    }
+
+</script>
+
+<script>
+    // Menghilangkan notifikasi setelah beberapa detik (misalnya, 5 detik)
+    setTimeout(function () {
+        $('.verifikasi-alert').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    }, 3000);
+
 </script>
 @endpush
